737329f9b2d7b80f39cd73e6fe4431e022355d92
Shuvo <meghsohor@yahoo.com>
2026-07-24 14:03:25 +0200

    Formatted remaining source files with Prettier

--- astro/src/config.ts
+++ astro/src/config.ts
@@ -1,6 +1,7 @@
 export const site = {
-  title: "MeghSohor",
-  description: "I am Shafiqul Islam Shuvo and I am a Software Developer. I write blog posts on HTML, CSS, SASS, JavaScript, Angular, React, Redux, Material, Bootstrap, TypeScript",
-  author: "@meghsohor",
-  siteURL: "https://www.meghsohor.dev",
-};
+  title: 'MeghSohor',
+  description:
+    'I am Shafiqul Islam Shuvo and I am a Software Developer. I write blog posts on HTML, CSS, SASS, JavaScript, Angular, React, Redux, Material, Bootstrap, TypeScript',
+  author: '@meghsohor',
+  siteURL: 'https://www.meghsohor.dev',
+}
