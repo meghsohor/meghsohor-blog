67302165375ce24c516f294a64169d8752aa7736
Shuvo <meghsohor@yahoo.com>
2026-07-24 14:03:25 +0200

    Added RSS feed, sitemap and robots.txt

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.meghsohor.dev',
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'dracula',
