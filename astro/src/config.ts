4d990107c79dd792dade43358b3675a0f6a21de8
Shuvo <meghsohor@yahoo.com>
2026-07-24 11:29:44 +0200

    Added base layout, SEO head and global styles with semantic theme tokens

---
+++ astro/src/config.ts
@@ -0,0 +1,6 @@
+export const site = {
+  title: "MeghSohor",
+  description: "I am Shafiqul Islam Shuvo and I am a Software Developer. I write blog posts on HTML, CSS, SASS, JavaScript, Angular, React, Redux, Material, Bootstrap, TypeScript",
+  author: "@meghsohor",
+  siteURL: "https://www.meghsohor.dev",
+};
