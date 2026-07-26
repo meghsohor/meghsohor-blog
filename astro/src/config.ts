af4980f49a40503fe3a4220875fabaf56fdf3074
Shuvo <meghsohor@yahoo.com>
2026-07-26 09:47:13 +0200

    Added JSON-LD (Person/WebSite/BlogPosting) and fixed duplicate og:type meta tags

--- astro/src/config.ts
+++ astro/src/config.ts
@@ -3,5 +3,12 @@ export const site = {
   description:
     'I am Shafiqul Islam Shuvo and I am a Software Developer. I write blog posts on HTML, CSS, SASS, JavaScript, Angular, React, Redux, Material, Bootstrap, TypeScript',
   author: '@meghsohor',
+  authorName: 'Shafiqul Islam Shuvo',
   siteURL: 'https://www.meghsohor.dev',
+  sameAs: [
+    'https://github.com/meghsohor',
+    'https://www.linkedin.com/in/meghsohor/',
+    'https://twitter.com/meghsohor',
+    'https://stackoverflow.com/users/7646942/shuvo',
+  ],
 }
